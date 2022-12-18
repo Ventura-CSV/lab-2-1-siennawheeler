e3d7852155f87aa3e1b0796cdd9ed0b265cde3ed
Kyu Lee <kwooler@gmail.com>
2022-12-18 01:13:53 -0800

    A21

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-class Names:
-    #################
-    # Implement the class Names
-    pass
-
-
 def main():
-    # Test code for your class
-    ca = Names('ca2021.txt')
-    print(ca)
-    fl = Names('fl2021.txt')
-    print(fl)
-    print(ca.nlist)
-    print(fl.nlist)
 
-    if ca > fl:
-        print('The name class ca is greater than fl')
-    else:
-        print('The name class fl is greater than or equal to ca')
+    regular = int(input('Enter the regular price: '))
+    rate = int(input('Enter the discount rate: '))
 
-    ca.printName('C', 'Count', True)
-    ca.printName('D', 'Name', False)
-    fl.printName('A', 'Count', True)
-    fl.printName('E', 'Name', False)
+    # regular = 100
+    # rate = 20
+
+    discount = regular * rate // 100
+    final = regular - discount
+    print(f'Regular Price: {regular}')
+    print(f'Discount Amount: {discount}')
+    print(f'Final Price: {final}')
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -1,54 +1,64 @@
-import random
 import main
+import io
+import sys
+import re
 
 
-def test_init():
-    ca = main.Names('ca2021.txt')
+def test_main_100_20():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '100 \n 20'
+    sys.stdin = io.StringIO(datastr)
 
-    lenofdict = len(ca._nlist)
-    assert lenofdict == 200, "Invalid value. Expected 200"
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+    # p = re.compile('[\w,\W]*100[\w,\W]*')
+    # res = p.match(lines[0])
+    # print(res.group())
+    res = re.search('100', lines[0])
+    assert res != None
+    print(res.group())
+    assert res.group() == '100', 'Expected 100'
 
-    cnt = len([d for d in ca.nlist if d['Gender'] == 'F'])
-    assert cnt == 100, "Invalid value. Expected 100"
-    cnt = len([d for d in ca.nlist if d['Gender'] == 'M'])
-    assert cnt == 100, "Invalid value. Expected 100"
+    res = re.search('20', lines[1])
+    assert res != None
+    print(res.group())
+    assert res.group() == '20', 'Expected 20'
 
-    mydict = [{'Name': 'Alexander', 'Count': 1483, 'Gender': 'M'}, {'Name': 'Amelia',
-                                                                    'Count': 1363, 'Gender': 'F'}, {'Name': 'Ava', 'Count': 1264, 'Gender': 'F'}]
-    ca._nlist = mydict
-    assert len(ca._nlist) == 3, "Expected 3"
-
-
-def test_str():
-    ca = main.Names('ca2021.txt')
-    print(ca)
-    assert ca.__str__ != None
-
-
-def test_gt():
-    ca = main.Names('ca2021.txt')
-    fl = main.Names('fl2021.txt')
-    result = ca > fl
-    assert result == True, 'Expected True'
-
-    mydict1 = [{'Name': 'Alexander', 'Count': 1483, 'Gender': 'M'}, {'Name': 'Amelia',
-                                                                     'Count': 1363, 'Gender': 'F'}, {'Name': 'Ava', 'Count': 1264, 'Gender': 'F'}]
-    ca.nlist = mydict1
-    mydict2 = [{'Name': 'Oliver', 'Count': 1523, 'Gender': 'M'}, {'Name': 'Luna', 'Count': 1458, 'Gender': 'F'}, {
-        'Name': 'Elijah', 'Count': 1498, 'Gender': 'M'}, {'Name': 'Amelia', 'Count': 1363, 'Gender': 'F'}]
-    fl.nlist = mydict2
-    result = ca > fl
-    assert result == False, 'Expected False'
+    res = re.search('80', lines[2])
+    assert res != None, 'The final price error'
+    print(res.group())
+    assert res.group() == '80', 'Expected 80'
 
 
-def test_printName():
-    ca = main.Names('ca2021.txt')
-    ret = ca.printName('C', 'Count', True)
-    assert ret == 10, 'Expected 10'
-    ret = ca.printName('E', 'Gender', False)
-    assert ret == 23, 'Expected 23'
-    mydict1 = [{'Name': 'Alexander', 'Count': 1483, 'Gender': 'M'}, {'Name': 'Amelia',
-                                                                     'Count': 1363, 'Gender': 'F'}, {'Name': 'Ava', 'Count': 1264, 'Gender': 'F'}]
-    ca.nlist = mydict1
-    ret = ca.printName('A', 'Count', True)
-    assert ret == 3, 'Expected 3'
+def test_main_200_40():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '200 \n 20'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+    # p = re.compile('[\w,\W]*100[\w,\W]*')
+    # res = p.match(lines[0])
+    # print(res.group())
+    res = re.search('200', lines[0])
+    assert res != None
+    print(res.group())
+    assert res.group() == '200', 'Expected 200'
+
+    res = re.search('40', lines[1])
+    assert res != None
+    print(res.group())
+    assert res.group() == '40', 'Expected 40'
+
+    res = re.search('160', lines[2])
+    assert res != None, 'The final price error'
+    print(res.group())
+    assert res.group() == '160', 'Expected 160'
