d02eb80d772d980996cb2d08fb572b0836e97b2d
Kyu Lee <kwooler@gmail.com>
2023-05-20 23:28:21 -0700

    Lab 2-1

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 def main():
-   ##################################################
-   # Comlete your code here
-   ##################################################
 
-    pass
+    original_price = int(input('Enter the regular price: '))
+    rate = int(input('Enter the discount rate: '))
+
+   ##################################################
+   # Comlete your code from here
+   ##################################################
+    discount_amount = 0     # Complete this expression to calculate the discount amount
+    final_price = 0         # Complete this expression to calculate the final price
+
+   ##################################################
+   # Do not delete the return statement
+   ##################################################
+    return original_price, discount_amount, final_price
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -10,28 +10,20 @@ def test_main_100_20():
     datastr = '100 \n 20'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    oprice, da, fprice = main.main()
     sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
+    print('Captured\n', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
     # p = re.compile('[\w,\W]*100[\w,\W]*')
     # res = p.match(lines[0])
     # print(res.group())
-    res = re.search('100', lines[0])
-    assert res != None
-    print(res.group())
-    assert res.group() == '100', 'Expected 100'
-
-    res = re.search('20', lines[1])
-    assert res != None
-    print(res.group())
-    assert res.group() == '20', 'Expected 20'
-
-    res = re.search('80', lines[2])
-    assert res != None, 'The final price error'
-    print(res.group())
-    assert res.group() == '80', 'Expected 80'
+    # res = re.search('100', lines[0])
+    # print(res.group())
+    # assert res.group() == '100', 'Expected 100'
+    assert oprice == 100, 'must be 100'
+    assert da == 20, 'must be 20'
+    assert fprice == 80, 'must be 80'
 
 
 def test_main_200_40():
@@ -40,25 +32,14 @@ def test_main_200_40():
     datastr = '200 \n 20'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    oprice, da, fprice = main.main()
     sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
+    print('Captured\n', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
     # p = re.compile('[\w,\W]*100[\w,\W]*')
     # res = p.match(lines[0])
     # print(res.group())
-    res = re.search('200', lines[0])
-    assert res != None
-    print(res.group())
-    assert res.group() == '200', 'Expected 200'
-
-    res = re.search('40', lines[1])
-    assert res != None
-    print(res.group())
-    assert res.group() == '40', 'Expected 40'
-
-    res = re.search('160', lines[2])
-    assert res != None, 'The final price error'
-    print(res.group())
-    assert res.group() == '160', 'Expected 160'
+    assert oprice == 200, 'must be 200'
+    assert da == 40, 'must be 40'
+    assert fprice == 160, 'must be 160'
